c3f819b35ac7d825c277a8ebbf7a533ee5b86233
Jijgee23 <94855075+Jijgee23@users.noreply.github.com>
2025-11-20 12:41:13 +0800

    2nd

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,17 +1,33 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<String>? tables = ['t1', 't2', 't3'];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Test app')),
-      body: Container(
-        padding: EdgeInsets.all(20),
-        decoration: BoxDecoration(color: Colors.red),
-        constraints: BoxConstraints(maxWidth: 200, maxHeight: 200),
-        child: Center(child: Text('MY FIRST WEB')),
+      appBar: AppBar(title: Text('Захилга'), backgroundColor: Colors.amber),
+      body: Center(
+        child: Container(
+          padding: EdgeInsets.all(20),
+          decoration: BoxDecoration(
+            color: Colors.teal,
+            gradient: LinearGradient(colors: [Colors.tealAccent, Colors.teal]),
+            image: DecorationImage(
+              fit: BoxFit.cover,
+              image: NetworkImage(
+                'https://www.tinbuilding.com/wp-content/uploads/2024/09/download-6-scaled-920x518.webp',
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
